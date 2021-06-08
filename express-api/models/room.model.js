b4e079e2e9ac504b36471f505cec455be9a10716
Saragam Ghimire <codewithghimire@gmail.com>
2021-06-08 18:08:16 +0545

    final commit

---
+++ express-api/models/room.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/*     var commentSchema = new Schema({
+        comment:String,
+        user:{
+            type:Schema.Types.ObjectId,
+            ref:'user'
+        }
+    }) */
+
+var roomSchema = new Schema({
+    price: Number,
+    image: [String],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    categories: {
+        type: String,
+        enum: ['premium', 'urgent', 'normal']
+    },
+    numberOfRoom: Number,
+    carParking: {
+        type: Boolean,
+        default: false
+    },
+    bikeParking: {
+        type: Boolean,
+        default: false
+    },
+    address: String,
+    description: String,
+    map: {
+        isMap: {
+            type: Boolean,
+            default: false
+        },
+        lat: Number,
+        lng: Number
+    },
+    comment: [{
+        comment: String,
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    }]
+}, {
+    timestamps: true
+})
+
+var roomModel = mongoose.model('room', roomSchema);
+
+module.exports = roomModel;
